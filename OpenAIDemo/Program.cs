d1fcd2fe8c259ce05a36de2d7835f4650608cd0a
Ugur <rextsts@hotmail.com>
2023-01-15 21:37:23 +0300

    Add project files.

---
+++ OpenAIDemo/Program.cs
@@ -0,0 +1,15 @@
+using OpenAI.GPT3.Extensions;
+using OpenAIDemo;
+using OpenAIDemo.Services;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddOpenAIService(settings => settings.ApiKey =
+        "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");
+        services.AddHostedService<OpenAICompletionService>();
+        services.AddHostedService<OpenAIImageService>();
+    })
+    .Build();
+
+host.Run();
---
+++ OpenAIDemo/Services/OpenAIImageService.cs
@@ -0,0 +1,37 @@
+﻿using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels.ResponseModels.ImageResponseModel;
+
+namespace OpenAIDemo.Services
+{
+    public class OpenAIImageService : BackgroundService
+    {
+
+        readonly IOpenAIService _openAIServices;
+
+        public OpenAIImageService(IOpenAIService openAIServices)
+        {
+            _openAIServices = openAIServices;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                Console.Write("::");
+                ImageCreateResponse result = await _openAIServices.Image.CreateImage(new ImageCreateRequest()
+                {
+                    Prompt = Console.ReadLine(),
+                    N = 1,// sonuç sayısı
+                    Size = StaticValues.ImageStatics.Size.Size256,
+                    ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
+                    User = "test"
+
+                });
+
+                Console.WriteLine(string.Join("\n", result.Results.Select(r => r.Url)));
+            }
+        }
+    }
+}
